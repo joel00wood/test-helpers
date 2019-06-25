3a6e675865433097b49000e29e844bcde7458312
joel00wood <joel.00.wood@gmail.com>
2019-06-25 21:57:47 +1000

    Created output capture
    Purpose of this is for reuse in my go-course labs.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # test-helpers
-test helper code
+
+Helper code for testing things in golang, maybe this exists elsewhere who knows.
+
+## capture
+
+### output
+
+captures output printed to stdout or similar by a given function.
+
+as inspired by: https://gist.github.com/hauxe/e935a7f9012bf2649710cf75af323dbf
---
+++ capture/output.go
@@ -0,0 +1,42 @@
+package output
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"sync"
+)
+
+func CaptureOutput(f func()) string {
+	reader, writer, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+	stdout := os.Stdout
+	stderr := os.Stderr
+	defer func() {
+		os.Stdout = stdout
+		os.Stderr = stderr
+		log.SetOutput(os.Stderr)
+	}()
+	os.Stdout = writer
+	os.Stderr = writer
+	log.SetOutput(writer)
+	out := make(chan string)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		var buf bytes.Buffer
+		wg.Done()
+		_, err := io.Copy(&buf, reader)
+		if err != nil {
+			panic(err)
+		}
+		out <- buf.String()
+	}()
+	wg.Wait()
+	f()
+	writer.Close()
+	return <-out
+}
